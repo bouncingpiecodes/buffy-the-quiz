1cd5b985d61d0f6bb78374277e53e4a287bd10f2
“kara <bouncingpie@icloud.com>
2022-09-21 23:18:19 -0400

    made buttons functional using javascript

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-function startTimer(duration, display) {
-  var timer = duration,
-    minutes,
-    seconds;
-  setInterval(function () {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    display.textContent = minutes + ":" + seconds;
-
-    if (--timer < 0) {
-      timer = duration;
-    }
-  }, 1000);
-}
-
-window.onload = function () {
-  var fiveMinutes = 60 * 5,
-    display = document.querySelector("#time");
-  startTimer(fiveMinutes, display);
+window.onload = () => {
+  document.getElementById("game-button").addEventListener("click", () => {
+    alert();
+  });
+  document.getElementById("play-button").addEventListener("click", () => {
+    alert();
+  });
 };
