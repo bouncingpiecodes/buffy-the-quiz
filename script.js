da85fc28d51aea45b963bedf374148f6fa2621bd
“kara <bouncingpie@icloud.com>
2022-09-27 22:07:54 -0400

    added local storage and JSON

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var seconds;
 var currentIndex = 0;
+var timer;
+var answersCorrect;
 window.onload = () => {
   document.getElementById("vhs-button").addEventListener("click", () => {
     alert();
@@ -14,14 +16,20 @@ window.onload = () => {
 };
 function startTimer() {
   seconds = 100;
-  setInterval(function () {
-    $(".timer").text(seconds);
-    seconds = seconds - 1;
+  answersCorrect = 0;
+  timer = setInterval(function () {
+    if (seconds < 0) {
+      clearInterval(timer);
+    } else {
+      $(".timer").text(seconds);
+      seconds = seconds - 1;
+    }
   }, 1000);
 }
 function answerQuestion(index) {
   if (index === questions[currentIndex].correctAnswerIndex) {
     alert("5 by 5!");
+    answersCorrect = answersCorrect + 1;
   } else {
     alert("Incorrect");
     seconds = seconds - 10;
@@ -30,8 +38,22 @@ function answerQuestion(index) {
     currentIndex = currentIndex + 1;
     askQuestion(currentIndex);
   } else {
+    clearInterval(timer);
+    $(".timer").addClass("hide");
     $(".ehs").removeClass("hide");
     $(".questions").addClass("hide");
+    alert(`You Scored ${answersCorrect}/${questions.length}`);
+  }
+}
+function saveScore() {
+  var initials = $("#initials").val();
+  var scores = localStorage.scores ? JSON.parse(localStorage.scores) : [];
+  if (!!initials) {
+    scores.push({
+      score: answersCorrect,
+      initials,
+    });
+    localStorage.setItem("scores", JSON.stringify(scores));
   }
 }
 
@@ -61,49 +83,6 @@ var questions = [
   },
   {
     question: "what season do we meet the anointed one?",
-    answers: ["Answer A", "Answer B", "Answer C", "Answer D"],
-    correctAnswerIndex: 1,
-  },
-  {
-    question:
-      "who said the line “that’s me favorite shirt, that’s me only shirt!”",
-    answers: ["Answer A", "Answer B", "Answer C", "Answer D"],
-    correctAnswerIndex: 1,
-  },
-  {
-    question: "what monster babe did xander NOT date?",
-    answers: ["Answer A", "Answer B", "Answer C", "Answer D"],
-    correctAnswerIndex: 1,
-  },
-  {
-    question: "what season do we meet the anointed one?",
-    answers: ["Answer A", "Answer B", "Answer C", "Answer D"],
-    correctAnswerIndex: 1,
-  },
-  {
-    question:
-      "who said the line “that’s me favorite shirt, that’s me only shirt!”",
-    answers: ["Answer A", "Answer B", "Answer C", "Answer D"],
-    correctAnswerIndex: 1,
-  },
-  {
-    question: "what monster babe did xander NOT date?",
-    answers: ["Answer A", "Answer B", "Answer C", "Answer D"],
-    correctAnswerIndex: 1,
-  },
-  {
-    question: "what season do we meet the anointed one?",
-    answers: ["Answer A", "Answer B", "Answer C", "Answer D"],
-    correctAnswerIndex: 1,
-  },
-  {
-    question:
-      "who said the line “that’s me favorite shirt, that’s me only shirt!”",
-    answers: ["Answer A", "Answer B", "Answer C", "Answer D"],
-    correctAnswerIndex: 1,
-  },
-  {
-    question: "what monster babe did xander NOT date?",
     answers: ["Answer A", "Answer B", "Answer C", "Answer D"],
     correctAnswerIndex: 1,
   },
