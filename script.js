5361cdc2ff4d9da83f5cc885f55455302e114c03
“kara <bouncingpie@icloud.com>
2022-09-27 20:26:33 -0400

    added the askQuestion function

--- script.js
+++ script.js
@@ -4,15 +4,26 @@ window.onload = () => {
   });
   document.getElementById("play-button").addEventListener("click", () => {
     $(".buttons").hide();
+    $(".timer").show();
+    $(".questions").removeClass("hide");
     startTimer();
+    askQuestion(currentIndex);
   });
 };
 function startTimer() {
-  var seconds = 60;
+  var seconds = 100;
   setInterval(function () {
     $(".timer").text(seconds);
     seconds = seconds - 1;
   }, 1000);
+}
+var currentIndex = 0;
+function askQuestion(index) {
+  $("#question").html(questions[index].question);
+  $("#answer-a").html(questions[index].answers[0]);
+  $("#answer-b").html(questions[index].answers[1]);
+  $("#answer-c").html(questions[index].answers[2]);
+  $("#answer-d").html(questions[index].answers[3]);
 }
 var questions = [
   {
